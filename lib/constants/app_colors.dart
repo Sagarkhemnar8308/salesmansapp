755be4d07b6fdb2a59fb175226d8d0e337784470
sagar81 <sagar_81>
2024-02-10 11:02:05 +0530

    changes

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color whitecolor = Colors.white;
+
+  static const Color borderColor = Color(0xff6B6B6B);
+
+  static const Color textGrey = Color(0xffD1D1D1);
+  static const Color backgroundDark = Color(0xff323340);
+}
---
+++ lib/presentation/payment_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:salesmanapp/constants/app_colors.dart';
+import 'package:salesmanapp/widgets/text_widget.dart';
+
+class PaymentScreen extends StatefulWidget {
+  const PaymentScreen({super.key});
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whitecolor,
+      appBar: AppBar(
+        backgroundColor: AppColors.whitecolor,
+        title:Text("New Order"),
+      ),
+      body: Column(
+        children: [
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                 height: 100,
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: "Search customer"),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(),
+                child: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              )
+            ],
+          ),
+          // Container(
+          //   height: 300,
+          //   width: MediaQuery.of(context).size.width,
+          //   decoration: BoxDecoration(color: Colors.white),
+          //   child: Column(
+          //     children: [
+          //       Text("Suresh Halter"),
+          //       Text("Phone Number"),
+          //       Text("9868627364"),
+          //       Row(
+          //         children: [
+          //           Column(
+          //             children: [
+          //               Text("Village Name"),
+          //               Text("Manori"),
+          //             ],
+          //           ),
+          //           Column(
+          //             children: [
+          //               Text("Pin code "),
+          //               Text("432611"),
+          //             ],
+          //           ),
+          //         ],
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          // Text("Products"),
+          // Row(
+          //   children: [
+          //     SizedBox(
+          //       height: 100,
+          //       width: 500,
+          //       child: TextFormField(
+          //         decoration: InputDecoration(hintText: "Search customer"),
+          //       ),
+          //     ),
+          //     Container(
+          //       height: 100,
+          //       width: 100,
+          //       decoration: BoxDecoration(),
+          //       child: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          //     )
+          //   ],
+          // ),
+          // ListView.builder(
+          //   itemBuilder: (context, index) {
+          //     return Container(
+          //       height: 100,
+          //       width: MediaQuery.of(context).size.width,
+          //       child: Row(
+          //         children: [
+          //           Container(
+          //             height: 100,
+          //             width: 200,
+          //             child: Text("Img"),
+          //           ),
+          //         ],
+          //       ),
+          //     );
+          //   },
+          // )
+        ],
+      ),
+    );
+  }
+}
